ccc121c7e031e0bf649d99a4857a3c00581b0247
Bruno de Oliveira <brunodeoliveira0409@gmail.com>
2025-04-02 19:37:11 -0300

    TESTE BRUNO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Component, computed } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { MenuComponent } from './components/layout/menu/menu.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: 'full'},
     {path: "login", component: LoginComponent},
-    {path: "TelaPrincipal", component: PrincipalComponent, children:[
+    {path: "menu", component: MenuComponent, children:[
 
         
     ]}
