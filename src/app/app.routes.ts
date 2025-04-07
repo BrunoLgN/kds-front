c9a054c1e67bb1eec74c7954d8e9d9a39421a377
XundaBoy <ARTHURROBERTBUCHMARQUES@GMAIL.COM>
2025-04-07 19:44:50 -0300

    login e registre-se

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { MenuComponent } from './components/layout/menu/menu.component';
+import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
+import { UsuarioFormComponent } from './components/usuarios/usuario-form/usuario-form.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: 'full'},
     {path: "login", component: LoginComponent},
-    {path: "menu", component: MenuComponent, children:[
+    {path: "admin", component: PrincipalComponent, children:[
+        {path: "dashboard", component: DashboardComponent},
+        {path: "cadastroUsuario", component: UsuarioFormComponent},
 
         
     ]}
--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -41,4 +41,8 @@ export class LoginComponent {
     });
   }
 
+  cadastro(){
+    this.router.navigate(["admin/cadastroUsuario"])
+  }
+
 }
