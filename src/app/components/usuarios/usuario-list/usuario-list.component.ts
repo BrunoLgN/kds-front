e406cc0e31c70b185cff0e2176f01bfc82c4ad8e
Bruno de Oliveira <brunodeoliveira0409@gmail.com>
2025-04-09 20:43:51 -0300

    design

--- src/app/components/usuarios/usuario-list/usuario-list.component.ts
+++ src/app/components/usuarios/usuario-list/usuario-list.component.ts
@@ -34,18 +34,29 @@ export class UsuarioListComponent {
 
     deleteById(usuario: Usuario){
 
-      if(confirm("Deseja deletar?")){
+      
+      Swal.fire({
+        title: "Você realmente deseja deletar o usuario?",
+        showDenyButton: true,
+        
+        confirmButtonText: "DELETAR",
+        denyButtonText: `NÃO DELETAR`
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.usuarioService.deleteById(usuario.id).subscribe({
+            next: (mensagem) =>{//DEU CERTO
+              Swal.fire(mensagem, '', 'success');
+              this.findAll();
+            },
+            error: (erro) =>{//DEU ERRADO
+              Swal.fire(erro.error, '', 'error');
+            }
+           });
+    }});
 
-        this.usuarioService.deleteById(usuario.id).subscribe({
-          next: (mensagem) =>{//DEU CERTO
-            Swal.fire(mensagem, '', 'success');
-            this.findAll();
-          },
-          error: (erro) =>{//DEU ERRADO
-            Swal.fire(erro.error, '', 'error');
-          }
-         });
+        
         
       }
     }
-}
+
