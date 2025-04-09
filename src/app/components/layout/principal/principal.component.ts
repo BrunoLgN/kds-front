586a15cf8bab574456c1b79f2f236510c221295b
Bruno de Oliveira <brunodeoliveira0409@gmail.com>
2025-04-08 22:12:31 -0300

    footer-dashboardInicial-imagem

--- src/app/components/layout/principal/principal.component.ts
+++ src/app/components/layout/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
