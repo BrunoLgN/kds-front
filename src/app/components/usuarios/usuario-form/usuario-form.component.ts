4cdc3a56abefbbcbbd539f4b5fe55723c2ae6dd1
XundaBoy <ARTHURROBERTBUCHMARQUES@GMAIL.COM>
2025-04-10 13:20:08 -0300

    teste de import no cadastro

--- src/app/components/usuarios/usuario-form/usuario-form.component.ts
+++ src/app/components/usuarios/usuario-form/usuario-form.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Usuario } from '../../../models/usuario';
 import { UsuarioService } from '../../../services/usuario.service';
 import Swal from 'sweetalert2';
 import { Cidade } from '../../../models/cidade';
+import { CidadeListComponent } from "../../cidades/cidade-list/cidade-list.component";
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -12,21 +14,74 @@ import { Cidade } from '../../../models/cidade';
 @Component({
   selector: 'app-usuario-form',
   standalone: true,
-  imports: [MdbFormsModule, FormsModule],
+  imports: [MdbFormsModule, FormsModule, CidadeListComponent],
   templateUrl: './usuario-form.component.html',
   styleUrl: './usuario-form.component.scss'
 })
 export class UsuarioFormComponent {
-  usuario: Usuario = new Usuario();
+  @Input() usuario: Usuario = new Usuario();
+  @Output() retorno = new EventEmitter();
   usuarioService = inject(UsuarioService);
+
+  rotaAtivida = inject(ActivatedRoute);
+  
+
+  construcutor(){
+    let id = this.rotaAtivida.snapshot.params["id"];
+    if(id>0){
+      this.findById(id);
+    }
+  }
+
+  findById(id: number){
+    this.usuarioService.findById(id).subscribe({
+      next: mensagem =>{
+        Swal.fire({
+          title: mensagem,
+          icon: "success",
+          confirmButtonText: "Ok"
+        });
+        this.usuario = new Usuario; // limpa o form
+      },
+      error: erro =>{
+        Swal.fire(erro.error, '', 'error');
+      }
+    })
+  }
   
 
   save(){
+    if(this.usuario.id>0){
     //instanciando na mao um ibjeto mas tem que ser autoincrement com o id do login
     let minhacidade = new Cidade();
     minhacidade.id = 2;;
     this.usuario.cidade = minhacidade;
     //termiino da instanciacao
+
+    
+    this.usuarioService.update(this.usuario, this.usuario.id).subscribe({
+      next: mensagem =>{
+       
+        Swal.fire({
+          title: mensagem,
+          icon: "error",
+          confirmButtonText: "Ok",
+
+        });
+
+        this.retorno.emit(`adskfljads`);
+  
+        },
+        error: erro =>{  
+          Swal.fire(erro.error, '', 'error');
+          this.retorno.emit(`adskfljads`);
+        }
+    })
+
+  }else{
+
+    
+    
 
     this.usuarioService.save(this.usuario).subscribe({
       next: mensagem =>{
@@ -41,6 +96,10 @@ export class UsuarioFormComponent {
         Swal.fire(erro.error, '', 'error');
       }
 
-    })
+    });
+  }
+  }
+  buscarCidade(){
+
   }
 }
--- src/app/components/usuarios/usuario-list/usuario-list.component.ts
+++ src/app/components/usuarios/usuario-list/usuario-list.component.ts
@@ -1,18 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Output, TemplateRef, ViewChild } from '@angular/core';
 import { Usuario } from '../../../models/usuario';
 import { UsuarioService } from '../../../services/usuario.service';
 import Swal from "sweetalert2";
+import { MdbModalModule, MdbModalRef, MdbModalService,  } from 'mdb-angular-ui-kit/modal';
+import { UsuarioFormComponent } from '../../usuarios/usuario-form/usuario-form.component';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
+
 
 
 
 @Component({
   selector: 'app-usuario-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MdbFormsModule, UsuarioFormComponent, MdbModalModule],
   templateUrl: './usuario-list.component.html',
   styleUrl: './usuario-list.component.scss'
 })
 export class UsuarioListComponent {
+  @Output () usuario = new Usuario;
+  //imports modal
+  modalService = inject(MdbModalService);//para conseguir abrir a modal
+  @ViewChild("modalCadastroUsuario") modalCadastroUsuario!: TemplateRef<any>;
+  modalRef!: MdbModalRef<any>;
+
+  usuarioEdit: Usuario = new Usuario();
 
     lista: Usuario[] = [];
     usuarioService = inject(UsuarioService);
@@ -58,5 +71,31 @@ export class UsuarioListComponent {
         
         
       }
+  gerarToast() {
+    return Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+  }
+
+  editUsuario(usuario: Usuario) {
+    this.usuarioEdit = Object.assign({}, usuario);
+    this.modalRef = this.modalService.open(this.modalCadastroUsuario);
+  }
+
+  retornoModal(mensagem:AnyCatcher){
+    this.modalRef.close();
+    this.findAll();
+  }
+  
+      
     }
+  
 
